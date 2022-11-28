df45863c3a60777060420fe9819442411a1f2a57
KJone <58522694+KJone1@users.noreply.github.com>
2022-11-28 20:31:21 +0200

    Add zerolog.

--- src/main.go
+++ src/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/KJone1/gophercises-task-manager/src/cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	if err := cmd.Execute(); err != nil {
-		log.Fatal("Failed to start")
+		log.Fatal().Msg("Failed to execute")
 	}
 }
