fd95cf8e5c986a6e79645aa34699115c254f8807
KJone <58522694+KJone1@users.noreply.github.com>
2022-12-03 21:46:11 +0200

    Update add command to write to bolt db.

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module github.com/KJone1/gophercises-task-manager
 go 1.19
 
 require (
+	github.com/jedib0t/go-pretty/v6 v6.4.3
 	github.com/rs/zerolog v1.28.0
 	github.com/spf13/cobra v1.6.1
+	go.etcd.io/bbolt v1.3.6
+
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 )
--- src/cmd/add.go
+++ src/cmd/add.go
@@ -1,44 +1,92 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
+	nanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/rs/zerolog/log"
-
 	"github.com/spf13/cobra"
+	bolt "go.etcd.io/bbolt"
 )
 
+type Task struct {
+	Id       string
+	isDone   bool
+	Task     string
+	Subtasks []string
+	Tag      string
+	Date     struct {
+		Year  int
+		Month time.Month
+		Day   int
+	}
+}
+
+func Save(db *bolt.DB, key string, value Task) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("Tasks"))
+		if err != nil {
+			return fmt.Errorf("failed to create bucket: %w", err)
+		}
+		encoded, err := json.Marshal(value)
+		if err != nil {
+			return fmt.Errorf("failed to parse task: %w", err)
+		}
+		err = bucket.Put([]byte(key), (encoded))
+		if err != nil {
+			return fmt.Errorf("failed to Write task to DB: %w", err)
+		}
+
+		return nil
+	})
+}
+
 var (
-	subtask = []string{}
-	tag     string
+	newtask = Task{}
 	addCmd  = &cobra.Command{
 		Use:   "add",
 		Short: "Add new task.",
 		Long:  "Add new task to task manager.",
 		Run: func(cmd *cobra.Command, args []string) {
-			task := strings.Join(args, " ")
-			// subtask := strings.Join(args, " ")
+			newtask.Task = strings.Join(args, " ")
+			newtask.Date.Year, newtask.Date.Month, newtask.Date.Day = time.Now().Date()
+			newtask.isDone = false
+			newtask.Id, _ = nanoid.Generate("12345678", 4)
 			if len(args) == 0 {
 				log.Error().Msg("No arguments provided for \"Add\" command")
 				os.Exit(1)
 			}
-			if len(subtask) == 0 {
-				fmt.Printf("Added task \"%s\" to [%s] list.\n", task, tag)
 
-				return
+			// It will be created if it doesn't exist.
+			db, err := bolt.Open("tasks.db", 0600, &bolt.Options{Timeout: 10 * time.Second})
+			if err != nil {
+				log.Fatal().Msgf("Failed to open DB: %w", err)
 			}
-			fmt.Printf("Added task \"%s\" to [%s] list with %d subtasks.\n", task, tag, len(subtask))
-			for i, k := range subtask {
-				fmt.Println(i, k)
+			defer db.Close()
+
+			err = Save(db, newtask.Id, newtask)
+			if err != nil {
+				log.Error().Msgf("Failed to Save task: %w", err)
 			}
-		},
-	}
+
+			switch len(newtask.Subtasks) {
+			case 0:
+				log.Info().Msgf("Added task \"%s\" to [%s] list.\n", newtask.Task, newtask.Tag)
+			case 1:
+				log.Info().Msgf("Added task \"%s\" and 1 subtask to [%s] list.  \n", newtask.Task, newtask.Tag)
+			default:
+				log.Info().Msgf("Added task \"%s\" and %d subtasks to [%s] list.  \n", newtask.Task, len(newtask.Subtasks), newtask.Tag)
+			}
+
+		}}
 )
 
 func init() {
-	addCmd.Flags().StringSliceVarP(&subtask, "subtask", "s", nil, "Add subtask")
-	addCmd.Flags().StringVarP(&tag, "tag", "t", "Inbox", "Tag your task")
+	addCmd.Flags().StringSliceVarP(&newtask.Subtasks, "subtask", "s", nil, "Add subtask")
+	addCmd.Flags().StringVarP(&newtask.Tag, "tag", "t", "Inbox", "Tag your task")
 	rootCmd.AddCommand(addCmd)
 }
