f6e1a7782211a618fca35944680773a0dc2de78c
KJone <58522694+KJone1@users.noreply.github.com>
2022-12-10 11:57:42 +0200

    Add --all flag.

--- src/cmd/ps.go
+++ src/cmd/ps.go
@@ -3,27 +3,52 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	bolt "go.etcd.io/bbolt"
 )
 
 var (
-	psCmd = &cobra.Command{
+	showAll bool
+	psCmd   = &cobra.Command{
 		Use:   "ps",
 		Short: "List task.",
 		Long:  "List tasks added to task manager.",
 		Run: func(cmd *cobra.Command, args []string) {
+			// Configure table params
 			t := table.NewWriter()
 			t.SetStyle(table.StyleDouble)
 			t.SetOutputMirror(os.Stdout)
-			t.AppendHeader(table.Row{"Task", "Sub Tasks", "Tag", "Date Added"})
+
+			if showAll {
+				t.AppendHeader(table.Row{"Task", "Sub Tasks", "Tag", "Date Added", "Status"})
+				t.SetColumnConfigs([]table.ColumnConfig{
+					{
+						Name:  "Sub Tasks",
+						Align: text.AlignLeft,
+					},
+				})
+			} else {
+				t.AppendHeader(table.Row{"Task", "Sub Tasks", "Tag", "Date Added"})
+				t.SetColumnConfigs([]table.ColumnConfig{
+					{
+						Name:  "Sub Tasks",
+						Align: text.AlignCenter,
+					},
+				})
+			}
+
 			// It will be created if it doesn't exist.
-			db, err := bolt.Open("tasks.db", 0600, &bolt.Options{Timeout: 10 * time.Second})
+			var filePermission fs.FileMode = 0600
+			timeout := 10 * time.Second
+			db, err := bolt.Open("tasks.db", filePermission, &bolt.Options{Timeout: timeout})
 			if err != nil {
 				log.Error().Msgf("Failed to open DB: %w", err)
 				os.Exit(1)
@@ -41,24 +66,58 @@ var (
 
 					task := Task{}
 					if err = json.Unmarshal(v, &task); err != nil {
-						log.Error().Msgf("failed to unmarshal: %w", err)
+						log.Error().Msgf("Failed to unmarshal: %w", err)
+						os.Exit(1)
+					}
+
+					if showAll {
+						date := fmt.Sprintf("%d/%d/%d", task.Date.Day, task.Date.Month, task.Date.Year)
+						subtasks := strings.Join(task.Subtasks, ", ")
+						var status string
+						if task.IsDone {
+							status = "Done"
+						} else {
+							status = "In Progress"
+						}
+						row := (table.Row{task.Task, subtasks, task.Tag, date, status})
+
+						t.AppendRow(row)
+						t.AppendSeparator()
+
+						return nil
+					}
+
+					if task.IsDone {
+						return nil
 					}
 
 					date := fmt.Sprintf("%d/%d/%d", task.Date.Day, task.Date.Month, task.Date.Year)
-					t.AppendRow(table.Row{task.Task, task.Subtasks, task.Tag, date})
+					var subtasksCount int
+					if len(task.Subtasks) == 0 {
+						subtasksCount = 0
+					} else {
+						subtasksCount = len(task.Subtasks)
+					}
+					row := (table.Row{task.Task, subtasksCount, task.Tag, date})
+
+					t.AppendRow(row)
 					t.AppendSeparator()
-					return err
+
+					return nil
 				})
+
 				t.Render()
-				return err
+
+				return nil
 			})
 			if err != nil {
-				log.Error().Msgf("failed to list tasks: %w", err)
+				log.Error().Msgf("Failed to list tasks: %w", err)
 			}
 		},
 	}
 )
 
 func init() {
+	psCmd.Flags().BoolVarP(&showAll, "all", "a", false, "Print all tasks")
 	rootCmd.AddCommand(psCmd)
 }
